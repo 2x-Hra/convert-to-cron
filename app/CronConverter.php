4fe36a53091c15aaef35db8378f0cf6c1d1c5526
Hosseinreza <hrakbari77@gmail.com>
2023-01-09 16:13:50 +0330

    Check output

--- app/CronConverter.php
+++ app/CronConverter.php
@@ -16,6 +16,20 @@ class CronConverter
      * output: "18 13 09 01 *"
      */
     public function toCronExpression($dateTime){
+        $separatedVar = explode(" ",$dateTime); // Separate the date time
+        $date = $separatedVar[0];
+        $time = $separatedVar[1];
+
+        $dateParts = explode("-", $date);
+        $day = $dateParts[2];
+        $month = $dateParts[1];
+
+        $timeParts = explode(":", $time);
+        $hour = $timeParts[0];
+        $minute = $timeParts[1];
+
+        $cron = $minute." ". $hour." " . $day." " . $month." " ."*";
+        echo $cron ;
 
     }
 }
