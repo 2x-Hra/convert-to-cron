aef2dd78050dd78b5dee38117ac07d846470b197
Hosseinreza <hrakbari77@gmail.com>
2023-01-09 16:13:25 +0330

    Add function

--- app/CronConverter.php
+++ app/CronConverter.php
@@ -9,5 +9,13 @@ class CronConverter
 
     }
 
+    /**
+     * Convert Date time to Cron Expression
+     *
+     * input : "2023-01-09 13:18:24"
+     * output: "18 13 09 01 *"
+     */
+    public function toCronExpression($dateTime){
 
+    }
 }
